4d9001fa61bddd33395d789f370387b6295efe22
iggy <iggynelo@gmail.com>
2024-05-15 12:42:55 +0900

    feat: implemented prompt llama3:80b method

--- plan-bench/utils/llm_utils.py
+++ plan-bench/utils/llm_utils.py
@@ -1,4 +1,7 @@
+import os
 import subprocess
+from groq import Groq
+from dotenv import load_dotenv
 
 def prompt_llama2(query):
     shellCommand = f"ollama run llama2 '{query}'"
@@ -18,6 +21,25 @@ def prompt_llama3(query):
     else:
         return result.stderr
 
+def prompt_llama3_80b(query):
+    load_dotenv()
+    client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+    result = client.chat.completions.create(
+        model="llama3-70b-8192",
+        messages=[
+            {
+                "role": "user",
+                "content": query
+            }
+        ],
+        temperature=1,
+        max_tokens=1024,
+        top_p=1,
+        stop=None,
+    )
+
+    return result.choices[0].message.content
+
 def send_query(query, engine, max_tokens, model=None, stop="[STATEMENT]"):
     if engine == "llama2":
         if model:
@@ -36,3 +58,7 @@ def send_query(query, engine, max_tokens, model=None, stop="[STATEMENT]"):
     else:
         print("invalid engine")
         return ""
+    
+if __name__ == "__main__":
+    response = prompt_llama3_80b("hi")
+    print(response)
