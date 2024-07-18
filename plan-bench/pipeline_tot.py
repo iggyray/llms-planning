29494db5d1219577af18ff4d7fcafd6a06d2f1d0
iggy <iggynelo@gmail.com>
2024-07-18 17:17:55 +0900

    feat: finalized bfs tot implementation for blocksworld 3

--- plan-bench/pipeline_tot.py
+++ plan-bench/pipeline_tot.py
@@ -8,7 +8,7 @@ from handler_report import report_handler
 from handler_validation import validation_handler
 from handler_nodes import node_db
 
-class base_tot_bfs_pipeline:
+class tot_blocksworld_3:
     def __init__(self, instance_number) -> None:
         load_dotenv()
         self.setup = setup_handler(instance_number)
@@ -26,7 +26,7 @@ class base_tot_bfs_pipeline:
         self.tot_queue = []
         self.prompt_number = None
     
-    def update_tot_state(self, prompt_report, llm_plan=None, reset_llm_plan=False):
+    def update_report(self, prompt_report, llm_plan=None, reset_llm_plan=False):
         updated_nodes = self.node_db.get_all_nodes()
         self.reporter.save_json_v2(self.prompt_number, prompt_report, updated_nodes, llm_plan, reset_llm_plan)
 
@@ -34,7 +34,7 @@ class base_tot_bfs_pipeline:
         pddl_action, _ = text_to_plan(llm_raw_response, self.problem.actions, "llm_plan", self.config)
         return get_action_description(self.config, pddl_action)
     
-    def get_formatted_actions_from_tot_response(self, prompt):
+    def get_formatted_actions_from_llm(self, prompt):
         retry_count = 0
         while retry_count < 3:
             formatted_action_list = []
@@ -59,23 +59,21 @@ class base_tot_bfs_pipeline:
                     self.validator.report_failed_instance('failed to get formatted action from tot response')
                     raise Exception('failed to get formatted action from tot response')
     
-    def tot_prompt(self):
+    def execute(self):
         if self.prompt_number is None:
-            self.prompt_number = 1
             cur_node_id = "0" # root node id is '0'
         else:
-            self.prompt_number += 1
             cur_node_id = self.tot_queue[0]
 
-        # check depth
+        # check if depth limit has been reached
         cur_node_depth = self.node_db.get_cur_node_depth(cur_node_id)
-        if cur_node_depth == self.depth_limit: # all nodes in depth_limit have been generated, no need TOT
+        if cur_node_depth == self.depth_limit:
             print('[INIT PLAN VALIDATOR]')
             llm_plans = []
             for node_id in self.tot_queue:
                 llm_plan = self.node_db.get_plan(node_id)
                 llm_plans.append(llm_plan)
-            self.update_tot_state({},  llm_plans, False)
+            self.update_report({},  llm_plans, False)
             self.validator.validate_llm_plans(llm_plans)
             return
             
@@ -86,7 +84,21 @@ class base_tot_bfs_pipeline:
             updated_init_state = self.node_db.get_state_from_id(cur_node_id)
             self.init_state, _ = self.setup.get_state_descriptions(updated_init_state)
 
-        # ToT
+        # action generation
+        self.action_generation_prompt(cur_node_id, cur_node_depth)
+
+        if len(self.tot_queue) == 0:
+            self.validator.report_failed_instance('tot queue empty')
+            return
+        
+        self.execute()
+        
+        
+    def action_generation_prompt(self, node_id, node_depth):
+        if self.prompt_number is None:
+            self.prompt_number = 1
+        else:
+            self.prompt_number += 1
         prompt_with_domain = self.domain_description + exp_bfs_init_tot_example + self.init_state + exp_bfs_init_tot_prompt
         reset_llm_plan = self.prompt_number == 1
         report = {
@@ -94,31 +106,25 @@ class base_tot_bfs_pipeline:
             "type": 'TOT',
             "prompt": prompt_with_domain,
         }
-        self.update_tot_state(report,  None, reset_llm_plan)
-        formatted_actions = self.get_formatted_actions_from_tot_response(prompt_with_domain)
+        self.update_report(report,  None, reset_llm_plan)
+        formatted_actions = self.get_formatted_actions_from_llm(prompt_with_domain)
         child_node_id_list = []
         for formatted_action in formatted_actions:
-            new_node_id = self.node_db.add_node(formatted_action, cur_node_id)
+            new_node_id = self.node_db.add_node(formatted_action, node_id)
             child_node_id_list.append(new_node_id)
         
         formatted_possible_actions_list = list(map(self.node_db.get_action_from_id, child_node_id_list))
-        print(f"[TOT] depth: {cur_node_depth} | " + ''.join(formatted_possible_actions_list).replace('\n', '; '))
+        print(f"[TOT] depth: {node_depth} | " + ''.join(formatted_possible_actions_list).replace('\n', '; '))
         report["response"] = formatted_possible_actions_list
-        self.update_tot_state(report,  None, reset_llm_plan)
-        
-        # validate and add to ToT queue
-        for node_id in child_node_id_list:
-                child_node_depth = self.node_db.get_cur_node_depth(node_id)
-                if child_node_depth == self.depth_limit:
-                    self.tot_queue.append(node_id)
-                else:
-                    self.evaluate_prompt(node_id)
+        self.update_report(report,  None, reset_llm_plan)
 
-        if len(self.tot_queue) > 0:
-            self.tot_prompt()
-        else:
-            self.validator.report_failed_instance('tot queue empty')
-            return
+        # add actions to ToT queue
+        for node_id in child_node_id_list:
+            target_node_action = self.node_db.get_action_from_id(node_id)
+            target_action = target_node_action.replace('\n', '')
+            gt_plan_actions = self.gt_plan.split('\n')
+            if target_action == gt_plan_actions[node_depth]:
+                self.tot_queue.append(node_id)
     
     def update_predicate_state_prompt(self, node_id):
         self.prompt_number += 1
@@ -150,40 +156,11 @@ class base_tot_bfs_pipeline:
             "action": node_action,
             "response": updated_state,
         }
-        self.update_tot_state(report)
-    
-    def evaluate_prompt(self, node_id):
-        node_action = self.node_db.get_action_from_id(node_id)
-        self.prompt_number += 1
-        eval_prompt = exp_bfs_vote_prompt_v2.format(node_action)
-        prompt_with_domain = self.domain_description + self.init_state + eval_prompt
-        retry_count = 0
-        while retry_count < 3:
-            response = prompt_llama3_80b(prompt_with_domain)
-            vote_line = response.splitlines()[-1].lower()
-            if ("valid action" in vote_line or "re-evaluate action" in vote_line):
-                break
-            else:
-                print('[RETRY EVAL]')
-                retry_count += 1
-        else:
-            raise Exception('vote prompt exceeded retries')
-        print('[EVAL] ' + vote_line)
-        report = {
-            "prompt_number": self.prompt_number,
-            "type": 'evaluate',
-            'action': node_action,
-            "prompt": eval_prompt,
-            "response": vote_line,
-        }
-        self.update_tot_state(report)
-        if ("re-evaluate action" in vote_line):
-            return
-        self.tot_queue.append(node_id)
+        self.update_report(report)
     
 if __name__=="__main__":
     target_instances = []
     for index, target_instance_number in enumerate(target_instances, start=1):
         print(f'[INSTANCE NUMBER]: {target_instance_number} || [PROGRESS]: {index} / {len(target_instances)}')
-        tot = base_tot_bfs_pipeline(target_instance_number)
-        tot.tot_prompt()
+        tot = tot_blocksworld_3(target_instance_number)
+        tot.execute()
